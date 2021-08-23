ea7cf46b634b84251708b52193966a636c65257c
XavierFix <javiereduardorobertiv@gmail.com>
2021-08-24 01:10:09 +0800

    added paypal support and refactor the backend and frontend

--- back-end/src/controllers/orderController.js
+++ back-end/src/controllers/orderController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const { Order } = require("../models/orders");
 const { User } = require("../models/user");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -224,3 +225,79 @@ module.exports = {
         res.json({ received: true });
     },
 };
+=======
+const { Order } = require('../models/orders')
+const { User } = require('../models/user')
+
+module.exports = {
+  async addOrderItems(req, res) {
+    const { cart } = req.body
+    const {
+      user,
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      shippingCost,
+      tax,
+      totalPrice,
+      itemsPrice,
+    } = cart
+
+    try {
+      if (!orderItems && orderItems.length === 0) {
+        throw new Error('No order items')
+      }
+      if (
+        !user ||
+        !user.email ||
+        !user.contactNum ||
+        !user.name ||
+        !user.surname
+      ) {
+        throw new Error('User Information is incomplete')
+      }
+
+      if (!shippingAddress) {
+        throw new Error('Shipping address is required')
+      }
+
+      if (
+        !paymentMethod ||
+        !shippingCost ||
+        !tax ||
+        !totalPrice ||
+        !itemsPrice
+      ) {
+        throw new Error(
+          'Some shipping information such as payment methods, shipping fees are missing',
+        )
+      }
+
+      const order = new Order(cart)
+
+      const createdOrder = await Order.create(order)
+
+      // Now add the order to user
+      const foundUser = await User.findById(user.userId)
+      foundUser.orders = foundUser.orders.concat(createdOrder._id)
+
+      await foundUser.save()
+
+      // send the created orders
+      res.status(201).send(createdOrder)
+    } catch (error) {
+      res.status(400).send({ message: error.message })
+    }
+  },
+  async getOrder(req, res) {
+    const orderId = req.params.id
+    try {
+      const order = await Order.findById(orderId)
+      res.send(order)
+    } catch (error) {
+      res.status(404).send({ message: error.message })
+    }
+  },
+  
+}
+>>>>>>> 095a905 (added paypal support and refactor the backend and frontend)
