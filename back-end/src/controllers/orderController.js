6a5343408a9c2f7af7c292bca6dec9ad1c0f394d
XavierFix <javiereduardorobertiv@gmail.com>
2021-08-29 22:11:55 +0800

    refactored product edit and create screen

--- back-end/src/controllers/orderController.js
+++ back-end/src/controllers/orderController.js
@@ -1,62 +1,68 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const { Order } = require("../models/orders");
 const { User } = require("../models/user");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+=======
+const { Order } = require('../models/orders')
+const { User } = require('../models/user')
+>>>>>>> 23f74ae (refactored product edit and create screen)
 
 module.exports = {
-    async addOrderItems(req, res) {
-        const { cart } = req.body;
-        const {
-            user,
-            orderItems,
-            shippingAddress,
-            paymentMethod,
-            shippingCost,
-            tax,
-            totalPrice,
-            itemsPrice,
-        } = cart;
+  async addOrderItems(req, res) {
+    const { cart } = req.body
+    const {
+      user,
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      shippingCost,
+      tax,
+      totalPrice,
+      itemsPrice,
+    } = cart
 
-        try {
-            if (!orderItems && orderItems.length === 0) {
-                throw new Error("No order items");
-            }
-            if (
-                !user ||
-                !user.email ||
-                !user.contactNum ||
-                !user.name ||
-                !user.surname
-            ) {
-                throw new Error("User Information is incomplete");
-            }
+    try {
+      if (!orderItems && orderItems.length === 0) {
+        throw new Error('No order items')
+      }
+      if (
+        !user ||
+        !user.email ||
+        !user.contactNum ||
+        !user.name ||
+        !user.surname
+      ) {
+        throw new Error('User Information is incomplete')
+      }
 
-            if (!shippingAddress) {
-                throw new Error("Shipping address is required");
-            }
+      if (!shippingAddress) {
+        throw new Error('Shipping address is required')
+      }
 
-            if (
-                !paymentMethod ||
-                !shippingCost ||
-                !tax ||
-                !totalPrice ||
-                !itemsPrice
-            ) {
-                throw new Error(
-                    "Some shipping information such as payment methods, shipping fees are missing"
-                );
-            }
+      if (
+        !paymentMethod ||
+        !shippingCost ||
+        !tax ||
+        !totalPrice ||
+        !itemsPrice
+      ) {
+        throw new Error(
+          'Some shipping information such as payment methods, shipping fees are missing',
+        )
+      }
 
-            const order = new Order(cart);
+      const order = new Order(cart)
 
-            const createdOrder = await Order.create(order);
+      const createdOrder = await Order.create(order)
 
-            // Now add the order to user
-            const foundUser = await User.findById(user.userId);
-            foundUser.orders = foundUser.orders.concat(createdOrder._id);
+      // Now add the order to user
+      const foundUser = await User.findById(user.userId)
+      foundUser.orders = foundUser.orders.concat(createdOrder._id)
 
-            await foundUser.save();
+      await foundUser.save()
 
+<<<<<<< HEAD
             // send the created orders
             res.status(201).send(createdOrder);
         } catch (error) {
@@ -283,6 +289,8 @@ module.exports = {
 
       await foundUser.save()
 
+=======
+>>>>>>> 23f74ae (refactored product edit and create screen)
       // send the created orders
       res.status(201).send(createdOrder)
     } catch (error) {
@@ -298,6 +306,29 @@ module.exports = {
       res.status(404).send({ message: error.message })
     }
   },
+<<<<<<< HEAD
   
 }
 >>>>>>> 095a905 (added paypal support and refactor the backend and frontend)
+=======
+
+  async deleteOrder(req, res) {
+    const orderId = req.params.id
+    try {
+      const order = await Order.findById(orderId)
+      await order.remove()
+      res.status(200).send('Successfully deleted order')
+    } catch (error) {
+      res.status(400).send({ message: error.message })
+    }
+  },
+  async getAllOrders(req, res) {
+    try {
+      const orders = await Orders.find({}).sort({ createdAt: -1 }).limit(8)
+      res.status(200).send(orders)
+    } catch (error) {
+      res.status(404).send({ message: error.message })
+    }
+  },
+}
+>>>>>>> 23f74ae (refactored product edit and create screen)
